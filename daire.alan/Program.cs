9718a697f37decd49694eb8d3fd806033c164e5f
mhammedyildirim <61433710+mhammedyildirim@users.noreply.github.com>
2020-03-07 23:23:01 +0300

    Initial commit

---
+++ daire.alan/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daire.alan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double yaricap = 0;
+            double area = 0;
+
+            Console.WriteLine("dairenin yaricapini giriniz: ");
+            yaricap = Convert.ToDouble(Console.ReadLine());
+
+            area = yaricap * 2 * 3.1416;
+
+            Console.WriteLine("dairenin alani {0} metredir.", area);
+
+            Console.ReadKey();
+        }
+    }
+}
